63d4e6f56f07e3c001f2ef360cef30dbb0458997
Álvaro P.R <51326422+Alvaro-P-R@users.noreply.github.com>
2020-04-22 19:03:50 +0200

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,29 @@
+from selenium import  webdriver
+from time import sleep
+from selenium.common.exceptions import  ElementClickInterceptedException
+from selenium.webdriver.common.keys import Keys
+
+web = ("https://www.jesusninoc.com/powershell/page/4/")
+
+driver = webdriver.Chrome()
+driver.maximize_window()	
+driver.get(web)	
+
+a = 1
+
+for i in range(100):
+	web = f'https://www.jesusninoc.com/powershell/page/{a}'
+	a +=1
+	for z in range(20):
+		
+		
+		driver.find_element_by_xpath('//*[@title="5 Stars"]').click()
+		sleep(2)
+	
+
+	
+
+	driver.get(web)
+	
+
+
